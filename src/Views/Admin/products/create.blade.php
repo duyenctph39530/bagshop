2329406ed2972e4d24ed780bed06faeb2f7d4efa
Ngo Van Hoang <ngovanhoang10102004@gmail.com>
2024-06-11 00:08:26 +0700

    CRUD Danh mục

--- src/Views/Admin/products/create.blade.php
+++ src/Views/Admin/products/create.blade.php
@@ -6,18 +6,17 @@ Thêm sản phẩm mới
 @section('content')
 <form action="{{url("admin/product/store")}}" method="post" enctype="multipart/form-data">
     <label for="">ID:</label>
-    <input type="text" name="id" class=" form-control" id="">
+    <input type="text" name="id" class=" form-control"   id="">
     <label for="">Tên sản phẩm:</label>
     <input type="text" name="name" class=" form-control" id="">
     <label for="">Giá:</label>
-    <input type="text" name="price" class=" form-control" id="">
+    <input type="text" name="price" class=" form-control"id="">
     <label for="">Ảnh:</label>
-    <input type="file" name="img" class=" form-control" id="">
+    <input type="file" name="img" class=" form-control"  id="">
     <select name="id_categories" id="">
         @foreach ($categoryPluck as $id => $name)
             <option value="{{$id}}">{{$name}}</option>
         @endforeach
-
     </select>
     <label for="description">Mô tả:</label>
     <input type="text" name="" class=" form-control" id="">
